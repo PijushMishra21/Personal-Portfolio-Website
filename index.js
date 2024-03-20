2b573f3a0a2a092ab1f62fe9c8a2583042d22b50
pijush mishra <pijushkantimishra971@gmail.com>
2024-03-21 01:04:03 +0530

    modified website

--- index.js
+++ index.js
@@ -44,37 +44,49 @@ window.onload = function() {
 
 
 
-let form = document.getElementById("contact_form");
     
-    async function handleSubmit(event) {
-      event.preventDefault();
-      let status = document.getElementById("my-form-status");
-      let data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status.innerHTML = "Thanks for your submission!";
-        //   form.reset()
-        form.textContent= "";
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status.innerHTML = "Oops! There was a problem submitting your form"
-            }
-          })
-        }
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
-    }
-    form.addEventListener("submit", handleSubmit);
+function clearForm(){
+  document.getElementById("contact_form").reset();
+}
+let contact_form = document.getElementById("contact_form");
+contact_form.addEventListener('submit',function (event){
+  // event.preventDefault();
+  clearForm();
+})
+
+
+
+
+
+//     async function handleSubmit(event) {
+//       event.preventDefault();
+//       let status = document.getElementById("my-form-status");
+//       let data = new FormData(event.target);
+//       fetch(event.target.action, {
+//         method: form.method,
+//         body: data,
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//       }).then(response => {
+//         if (response.ok) {
+//           status.innerHTML = "Thanks for your submission!";
+//         //   form.reset()
+//         form.textContent= "";
+//         } else {
+//           response.json().then(data => {
+//             if (Object.hasOwn(data, 'errors')) {
+//               status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+//             } else {
+//               status.innerHTML = "Oops! There was a problem submitting your form"
+//             }
+//           })
+//         }
+//       }).catch(error => {
+//         status.innerHTML = "Oops! There was a problem submitting your form"
+//       });
+//     }
+//     contact_form.addEventListener("submit", handleSubmit);
 
       
 
@@ -105,5 +117,9 @@ function toggleDarkMode() {
     console.log('Dark mode preference set to false'); // Check if this message is logged in the console
   }
 }
+
+
+
+
 
 
